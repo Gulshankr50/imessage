d9578ea4f9a6680503a2a7e5d72d07a9a38f967f
Gulshankr50 <gulshanpbr8@gmail.com>
2026-06-17 01:44:53 +0530

    Add Clerk webhook handler and update server configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,18 @@ import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
 
-app.use(express.json())
+// its important that you dont parse the webhook event data, it should be in the raw format
+app.use("/api/webhooks/clerk", express.raw({type: "application/json"}), clerkWebhook);
+
+app.use(express.json());
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
 app.use(clerkMiddleware());
 
